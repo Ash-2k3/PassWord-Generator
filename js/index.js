62b202ddb5151473d6bc7c59cb23c6e5c838e666
Ash-2k3 <ash.me.cse@gmail.com>
2023-01-15 20:14:10 +0530

    Variables selected + slight modifications in css and html

---
+++ js/index.js
@@ -0,0 +1,39 @@
+
+/* Generated password section */ 
+var passwordGenerated = document.querySelector("section.generated-password");
+// console.log(passwordGenerated.innerText);
+
+/* Character-length numeric */ 
+var passLength = document.querySelector(".num-length");
+// console.log(passLength.innerText);
+
+/* Password length slider */
+var lengthSlider = document.querySelector("input[type=range");
+// console.log(lengthSlider.value);
+
+/*CheckBoxes */
+var isUppercaseChecked = document.querySelector("input#uppercase");
+// console.log(isUppercaseChecked.checked = true);
+
+var isLowercaseChecked = document.querySelector("input#lowercase");
+// console.log(isLowercaseChecked.checked = false)
+
+var isNumberChecked = document.querySelector("input#numbers");
+// console.log(isNumberChecked.checked);
+
+var isSymbolsAllowed = document.querySelector("input#symbols");
+// console.log(isSymbolsAllowed.checked=true);
+
+/* Password Strength */
+var passStrength = document.querySelector(".password-strength");
+// console.log(passStrength);
+
+/* Generate Button */
+const generateBtn = document.querySelector("button.generate");
+// console.log(generateBtn);
+
+
+
+
+
+
